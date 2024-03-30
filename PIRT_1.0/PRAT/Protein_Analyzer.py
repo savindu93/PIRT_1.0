54f23f5322d0478c99ba8266455d25f9443397bf
savindu93 <143857805+savindu93@users.noreply.github.com>
2024-03-30 10:44:41 +0530

    updated

--- PIRT_1.0/PRAT/Protein_Analyzer.py
+++ PIRT_1.0/PRAT/Protein_Analyzer.py
@@ -29,7 +29,7 @@ class PRAT:
 
         for ID in IDs:
             st.write(ID)
-            handle = ExPASy.get_sprot_raw(ID)
+            handle = ExPASy.get_sprot_raw(ID.strip())
             protein_data.append(SwissProt.parse(handle))
 
         filenames = []
